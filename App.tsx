e2c646e680770db61a8de44d90a16fb54c7a72e6
barackj370-pixel <barackj370@gmail.com>
2026-03-29 15:50:05 +0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -11,6 +12,17 @@ import KeywordGeneratorPage from './pages/KeywordGeneratorPage';
 import ContentGeneratorPage from './pages/ContentGeneratorPage';
 import FunnelBuilderPage from './pages/FunnelBuilderPage';
 import PublishedFunnel from './pages/PublishedFunnel';
+
+// New Pages
+import CustomAppDevPage from './pages/CustomAppDevPage';
+import WebDevelopmentPage from './pages/WebDevelopmentPage';
+import DigitalStrategyPage from './pages/DigitalStrategyPage';
+import UIUXDesignPage from './pages/UIUXDesignPage';
+import AboutUsPage from './pages/AboutUsPage';
+import PortfolioPage from './pages/PortfolioPage';
+import CareersPage from './pages/CareersPage';
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+import TermsOfServicePage from './pages/TermsOfServicePage';
 
 function App() {
   return (
@@ -28,6 +40,19 @@ function App() {
             <Route path="/tools/content-generator" element={<ContentGeneratorPage />} />
             <Route path="/tools/funnel-builder" element={<FunnelBuilderPage />} />
             <Route path="/f/:slug" element={<PublishedFunnel />} />
+            
+            {/* Services Routes */}
+            <Route path="/services/custom-app-development" element={<CustomAppDevPage />} />
+            <Route path="/services/web-development" element={<WebDevelopmentPage />} />
+            <Route path="/services/digital-strategy" element={<DigitalStrategyPage />} />
+            <Route path="/services/ui-ux-design" element={<UIUXDesignPage />} />
+
+            {/* Company Routes */}
+            <Route path="/company/about-us" element={<AboutUsPage />} />
+            <Route path="/company/portfolio" element={<PortfolioPage />} />
+            <Route path="/company/careers" element={<CareersPage />} />
+            <Route path="/company/privacy-policy" element={<PrivacyPolicyPage />} />
+            <Route path="/company/terms-of-service" element={<TermsOfServicePage />} />
           </Routes>
         </main>
 
